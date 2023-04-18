5b3cf0bee9bff0b3d4c70e8040fac9e654298452
AnnabBru <130755006+AnnabBru@users.noreply.github.com>
2023-04-18 13:32:04 +0200

    Update SVM_validate.py
    Dataset creation considering the position in a given contig/chromosome. Also modification of the position taken to get the potential pseudoU in the middle of the k--mer and not in the first place of the k-mer.

--- SVM_validate.py
+++ SVM_validate.py
@@ -16,10 +16,10 @@ import plot_learning_curves as plc
 from sklearn.preprocessing import MinMaxScaler #For feature normalization
 scaler = MinMaxScaler()
 
-df1 = pd.read_csv("Pseu_Modification_coors_ns_hek.txt",sep=' ',skiprows=(0),header=(0))
-df2 = pd.read_csv("xaa.txt",sep='\t',skiprows=(0),header=(0))
-df3 = pd.read_csv("Pseu_Modification_coors_hela.txt",sep=' ',skiprows=(0),header=(0))
-df4 = pd.read_csv("hela-reads-ref.eventalign.txt",sep='\t',skiprows=(0),header=(0))
+df1 = pd.read_csv("Pseu_Modification_coors.txt",sep=' ',skiprows=(0),header=(0))
+df2 = pd.read_csv("eventalign_train.txt",sep='\t',skiprows=(0),header=(0))
+df3 = pd.read_csv("Pseu_Modification_coors.txt",sep=' ',skiprows=(0),header=(0))
+df4 = pd.read_csv("eventalign_test.txt",sep='\t',skiprows=(0),header=(0))
 
 print(df2.shape)
 print("&&&&&&&&")
@@ -30,16 +30,7 @@ print("######################")
 print(df3.head())
 print("######################")
 print(df4.head())
-#get the 2nd column of 
-#column_2 = df1.iloc[:, 1]
 
-#print(df1['position'].iloc[0:5])
-print(df1.iloc[0:5, 1])
-print("@@@@@@@@@@@@@@@")
-#print(df2['position'].iloc[0:5])
-#print(df2.iloc[0:5, 1])
-print(df2.iloc[0:5, 9])
-print("######################")
 model_kmer_list=list(df2.iloc[:, 9]) #10 for model-kmer that
 model_kmer_list_test=list(df4.iloc[:, 9]) #10 for model-kmer that
 print("333333333333333333", type(model_kmer_list))
@@ -48,6 +39,24 @@ print(model_kmer_list[5])
 print(model_kmer_list[5][2])
 print(model_kmer_list_test[5])
 print(model_kmer_list_test[5][2])
+
+def filter_df(df1,df2,chrom):
+    df_pseu = pd.DataFrame()
+    df_U = pd.DataFrame()
+    for el in chrom :
+        df1_sub = df1[df1.iloc[:,0] == el]
+        df2_sub = df2[df2["contig"] == el]
+        x_sub = list(set(df1_sub.iloc[:,1]-2).intersection(set(df2_sub.iloc[:,1])) )
+        df_pseu_sub = df2_sub[df2_sub['position'].isin(x_sub)]
+        df_pseu = df_pseu.append(df_pseu_sub)
+        df_U_sub = df2_sub[~df2_sub['position'].isin(x_sub)]
+        df_U = df_U.append(df_U_sub)
+
+    return df_pseu, df_U
+
+chrom_train = list(set(df1.iloc[:,0]))
+chrom_test = list(set(df3.iloc[:,0]))
+
 U_kmer_list=[]
 for i in model_kmer_list:
     #print(i)
@@ -57,90 +66,57 @@ for i in model_kmer_list:
 U_kmer_list_test=[]
 for i in model_kmer_list_test:
     #print(i)
-    if i[2]=='T':
+    if i[2]=='T' :
         U_kmer_list_test.append(i)
-print("length of U_kmer_list",len(U_kmer_list))
-print(U_kmer_list[0:50])
-
-print("length of U_kmer_list_test",len(U_kmer_list_test))
-print(U_kmer_list_test[0:50])
 
 df=df2[df2['model_kmer'].isin(U_kmer_list)]
-print(df.shape)
-print(df.head())
+df_test = df4[df4['model_kmer'].isin(U_kmer_list_test)]
 
-np.savetxt('filtered_df_U_kmer', df,fmt='%s')
+df_pseu,df_U = filter_df(df1,df,chrom_train)
+df_pseu_test,df_U_test = filter_df(df3,df_test,chrom_test)
 
+#df_pseu = df_pseu.sample(n=2000, replace=False)#For Arabidopsis
 
-df_test=df4[df4['model_kmer'].isin(U_kmer_list_test)]
-print(df_test.shape)
-print(df_test.head())
-
-np.savetxt('filtered_df_U_kmer_test', df_test,fmt='%s')
-
-
-
-#label the data
-x=list(set(df1.iloc[:,1]).intersection(set(df.iloc[:,1])))
-print("length of intersection list",len(x))
-
-x_test=list(set(df3.iloc[:,1]).intersection(set(df_test.iloc[:,1])))
-print("length of intersection list",len(x_test))
-
-
-
-df_pseu=df[df['position'].isin(x)]
 listofones = [1] * len(df_pseu.index)
-# Using DataFrame.insert() to add a column 
 df_pseu.insert(13, "label", listofones, True)
 
-df_pseu_test=df_test[df_test['position'].isin(x_test)]
 listofones = [1] * len(df_pseu_test.index)
-# Using DataFrame.insert() to add a column 
 df_pseu_test.insert(13, "label", listofones, True)
 
-
-df_U=df[~df['position'].isin(x)]
 listofzeros=[0]*len(df_U.index)
 df_U.insert(13, "label", listofzeros, True)
-print(df_pseu.shape)
-print(df_pseu.head())
-print(df_U.shape)
-print(df_U.head())
 
-
-df_U_test=df_test[~df_test['position'].isin(x_test)]
 listofzeros=[0]*len(df_U_test.index)
 df_U_test.insert(13, "label", listofzeros, True)
-print(df_pseu_test.shape)
-print(df_pseu_test.head())
-print(df_U_test.shape)
-print(df_U_test.head())
-#np.savetxt('pseu_samples.txt', df_pseu,fmt='%s')
-#np.savetxt('U_samples.txt', df_U,fmt='%s')
-##########prepare training datast       
-df_U = df_U.sample(n=len(df_pseu), replace=False) #try replace=false
 
+##########prepare training datast  
+   
+df_U = df_U.sample(n=len(df_pseu), replace=False) #try replace=false
 # Create DataFrame from positive and negative examples
 dataset = df_U.append(df_pseu, ignore_index=True)
+print('DATASET :',dataset.head())
 #dataset['label'] = dataset['label'].astype('category')
 columns=['event_level_mean','event_stdv','event_length']
 #columns=['event_level_mean']
 #columns=['event_stdv']
 #columns=['event_length']
 
-
 ##################################
 #prepare testing datast       
-df_U_test = df_U_test.sample(n=len(df_pseu_test), replace=False) #try replace=false
+####df_U_test = df_U_test.sample(n=len(df_pseu_test), replace=False) #try replace=false
+
+#np.savetxt('pseu_samples.txt', df_pseu_test,fmt='%s')
+#np.savetxt('U_samples.txt', df_U_test,fmt='%s')
 
 # Create DataFrame from positive and negative examples
 dataset_test = df_U_test.append(df_pseu_test, ignore_index=True)
+
 #dataset_test['label'] = dataset_test['label'].astype('category')
 
 #shuffle the test and train datasets
 from sklearn.utils import shuffle
 dataset = shuffle(dataset)
+dataset_test.to_csv("PreShuffle.csv",index=False)
 dataset_test=shuffle(dataset_test)
 
 #combine onehot_encoding of train and test
@@ -158,6 +134,7 @@ X_train= pd.concat([X_train,Onehot],axis=1)
 #X_train=Onehot
 print("#############",X_train.shape)
 print(X_train.head())
+
 #scale training data
 X_train= scaler.fit_transform(X_train)
 y_train = dataset['label'] 
@@ -172,9 +149,11 @@ X_test= pd.concat([X_test,Onehot],axis=1)
 
 print("#############",X_test.shape)
 print(X_test.head())
+
 #scale training data
 X_test= scaler.fit_transform(X_test)
 y_test = dataset_test['label'] 
+
 print(",,,,,,,,",X_test.shape)
 
 ###################################
@@ -183,7 +162,6 @@ from sklearn.model_selection import train_test_split
 
 #train, test = train_test_split(df, test_size=0.2)   
 #X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
-
 #X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.3) for unblanced dataset
 
 #clf = classifier.fit(X_train,y_train)
@@ -212,8 +190,54 @@ y_pred = classifier.predict(X_test)
 y_prob = classifier.predict_proba(X_test)
 y_prob = y_prob[:,1]
 
+def fill_tp_fp_fn(l,dataset):
+    df = list()
+    dico = {}
+
+    for el in l :
+        df.extend([dataset.iloc[el][0],str(dataset.iloc[el][1]+2)])
+
+    df = ['_'.join(df[i:i+2]) for i in range(0,len(df),2)]
+    for el in df :
+        
+        if el not in dico :
+            dico[el] = 1
+        else : 
+            dico[el] = dico[el] + 1
+    df = list(set(df))
+    return df, dico
+
+l_fp,l_tp,l_fn,l_tn = list(),list(),list(),list()
+for i in range(len(y_pred)):
+    if y_pred[i] == 1 and y_test.iloc[i] == 1 :
+        l_tp.append(i)
+    elif y_pred[i] == 1 and y_test.iloc[i] == 0 :
+        l_fp.append(i)
+    elif y_pred[i] == 0 and y_test.iloc[i] == 1 :
+        l_fn.append(i)
+    else : 
+        l_tn.append(i)
+
+tp_pos,d_tp = fill_tp_fp_fn(l_tp,dataset_test)
+fp_pos,d_fp = fill_tp_fp_fn(l_fp,dataset_test)
+fn_pos,d_fn = fill_tp_fp_fn(l_fn,dataset_test)
+tn_pos, d_tn = fill_tp_fp_fn(l_tn,dataset_test)
+
+
+
+print('TP_freq', d_tp)
+print('#############')
+
+print('FP_freq', d_fp)
+print('#############')
+
+print('FN_freq', d_fn)
+print('#############')
+
+#print('TN_freq', d_tn)
+#print('#############')
+
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred)*100)
- 
 print(classification_report(y_test, y_pred))
 auc=roc_auc_score(y_test.round(),y_pred)
 auc = float("{0:.3f}".format(auc))
@@ -222,48 +246,63 @@ print("AUC=",auc)
 #tn c00, fpC01, fnC10, tpC11 
 print('CF=',confusion_matrix(y_test, y_pred))
 l=confusion_matrix(y_test, y_pred)#https://towardsdatascience.com/accuracy-precision-recall-or-f1-331fb37c5cb9
+
+
 print('TN=',l.item((0, 0)))
 print('FP=',l.item((0, 1)))
 print('FN=',l.item((1, 0)))
 print('TP=',l.item((1, 1)))
-#print(type(X_train), type(y_train))
+
+print("////////////////////")
+print("Error Rate TRAIN")
+
+y_pred_train = classifier.predict(X_train)
+y_prob_train = classifier.predict_proba(X_train)
+y_prob_train = y_prob_train[:,1]
+
+
+l_fp,l_tp,l_fn,l_tn = list(),list(),list(),list()
+for i in range(len(y_pred_train)):
+    if y_pred_train[i] == 1 and y_train.iloc[i] == 1 :
+        l_tp.append(i)
+    elif y_pred_train[i] == 1 and y_train.iloc[i] == 0 :
+        l_fp.append(i)
+    elif y_pred_train[i] == 0 and y_train.iloc[i] == 1 :
+        l_fn.append(i)
+    else : 
+        l_tn.append(i)
+
+
+tp_pos = fill_tp_fp_fn(l_tp,dataset)
+fp_pos = fill_tp_fp_fn(l_fp,dataset)
+fn_pos = fill_tp_fp_fn(l_fn,dataset)
+tn_pos = fill_tp_fp_fn(l_tn,dataset)
+
+print('TP : ', tp_pos)
+print('FP : ', fp_pos)
+print('FN : ', fn_pos)
+print('TN : ', tn_pos)
+
+print("Accuracy:",metrics.accuracy_score(y_train, y_pred_train)*100)
+ 
+print(classification_report(y_train, y_pred_train))
+auc=roc_auc_score(y_train.round(),y_pred_train)
+auc = float("{0:.3f}".format(auc))
+print("AUC=",auc)
+
+#true negatives c00, false negatives C10, true positives C11, and false positives C01 
+
+#tn c00, fpC01, fnC10, tpC11 
+print('CF=',confusion_matrix(y_test, y_pred))
+l=confusion_matrix(y_train, y_pred_train)#https://towardsdatascience.com/accuracy-precision-recall-or-f1-331fb37c5cb9
+print('TN=',l.item((0, 0)))
+print('FP=',l.item((0, 1)))
+print('FN=',l.item((1, 0)))
+print('TP=',l.item((1, 1)))
 
 
 #plot learning curve: works with all classifier and all features except x(padded signal) as it leads to error with SVM 
 #References:https://medium.com/@datalesdatales/why-you-should-be-plotting-learning-curves-in-your-next-machine-learning-project-221bae60c53
-import matplotlib.pyplot as plt
-
-
-plc. plot_learning_curves(classifier, X_train, y_train, X_test, y_test)
-
-# Create plot
-#plt.title("Learning Curve")
-plt.xlabel("Training Set Size"), plt.ylabel("Accuracy Score"), plt.legend(loc="best")
-plt.tight_layout()
-plt.savefig('SVM_validation_onehot_LC.png',dpi=300)
-plt.savefig('SVM_validation_onehot_LC.svg')
-plt.close()
-
-
-#plot ROC curve: https://stackoverflow.com/questions/25009284/how-to-plot-roc-curve-in-python
-from sklearn import metrics
-import numpy as np
-import matplotlib.pyplot as plt
-
-fpr, tpr, thresholds = metrics.roc_curve(y_test, y_prob)
-
-
-# Print ROC curve
-plt.plot(fpr,tpr)
-#plt.title("ROC Curve")
-# axis labels
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.savefig('SVM_validation_onehot_ROC.png',dpi=300)
-plt.savefig('SVM_validation_onehot_ROC.svg')
-
-plt.close() 
-
 
 #############################################
 #old code to plot learning curve: works only with RandomForest
